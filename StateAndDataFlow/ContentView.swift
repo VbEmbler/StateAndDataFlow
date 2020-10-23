af15c64f48c6135f17ec0c453aa80d8afd582a51
Embler <vbembler@gmail.com>
2020-10-23 16:08:34 +0300

    Initial Commit

---
+++ StateAndDataFlow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StateAndDataFlow
+//
+//  Created by Vladimir on 23/10/2020.
+//  Copyright © 2020 Embler. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
